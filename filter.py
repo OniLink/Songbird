1e2e962294cb7637008b932cdcef2003e88fcb95
Violet Kurtz <vi.kurtz@protonmail.com>
2022-02-12 21:21:41 -0800

    Added signal leveling and tweaked filter width

--- filter.py
+++ filter.py
@@ -6,28 +6,42 @@ import scipy.signal as sig
 from scipy.io import wavfile
 
 
-def data_filter(data, sample_rate):
-    data = data[:,0] # Take the left channel, discard the unused right
-    # TODO: Replace above with sophisticated test? Sum both channels?
+def normalize_signal(data):
+    return data / data[np.argmax(data)]
+
+
+def extract_am_signal(data, sample_rate):
+    filt_order = 3
+    filter_width = 50
     freq, pwelch_spec = sig.welch(data, sample_rate, scaling='spectrum')
 
-    # Select peak frequency and then bandpass filter the signal
-    filt_order = 3
-    filter_width = 0.001
-
+    # Extract peak frequency
     peak_freq = freq[np.argmax(pwelch_spec)]
     peak_nyq = peak_freq / (sample_rate / 2.0)
-    butter_sos = sig.butter(filt_order, [(1 - filter_width) * peak_nyq, (1 + filter_width) * peak_nyq], btype='band', output='sos')
-    filtered_signal = sig.sosfilt(butter_sos, data)
+    width_nyq = filter_width / (sample_rate / 2.0)
+
+    # Bandpass just that frequency
+    butter_sos = sig.butter(filt_order, [peak_nyq - width_nyq, peak_nyq + width_nyq], btype='band', output='sos')
 
     # Plot freq spectrum
-    plt.semilogy(freq, pwelch_spec)
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('PSD')
-    plt.grid()
-    plt.show()
+    #plt.semilogy(freq, pwelch_spec)
+    #plt.xlabel('Frequency [Hz]')
+    #plt.ylabel('PSD')
+    #plt.grid()
+    #plt.show()
 
-    return filtered_signal, sample_rate
+    return sig.sosfilt(butter_sos, data)
+
+
+def data_filter(data, sample_rate):
+    signal = data[:,0].astype(np.float32) # Take the left channel, discard the unused right
+    # TODO: Replace above with sophisticated test? Sum both channels?
+
+    signal = normalize_signal(signal)
+    signal = extract_am_signal(signal, sample_rate)
+    signal = normalize_signal(signal)
+
+    return signal, sample_rate
 
 
 def main():
