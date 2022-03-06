b4382df09d68fe853776fe73ac3b5596eec23639
K-Concept <Kagaminelove02@gmail.com>
2022-03-06 12:59:47 -0800

    Updated BeatCount.py
    It is more modular, changed height to prominence

--- BeatCount.py
+++ BeatCount.py
@@ -1,36 +1,54 @@
 from os.path import dirname, join as pjoin
 from scipy.io import wavfile
 from scipy import signal
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks, peak_prominences
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 #code referenced https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
 #code referenced https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
 
-#read from directory
-data_dir = pjoin(dirname(__file__), 'heartbeat-01a.wav')
+#This display orange xs where the peaks are 
+def display_peaks(peaks,beat):
+    plt.plot(beat, label="Left channel")
 
-#read wav
-samplerate, data = wavfile.read(data_dir)
+    #orange xs
+    plt.plot(peaks, beat[peaks], "x")
+    #right channel? just incase idk
+    #plt.plot(data[:, 1], label="Right channel")
+    plt.legend()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Amplitude")
+    plt.show()
 
-#  data stuff
-#  print(f"number of channels = {data.shape[1]}")
-#  length = data.shape[0] / samplerate
-#  print(f"length = {length}s")
+#takes in data, distance, prominence
+def peak_count(data,distance,prominence):
+    #  data stuff
+    #  print(f"number of channels = {data.shape[1]}")
+    #  length = data.shape[0] / samplerate
+    #  print(f"length = {length}s")
 
-#left array 
-beat = data[:, 0]
-peaks,_ = find_peaks(beat, distance=2000, height=1000000000)
-np.diff(peaks)
-
-plt.plot(beat, label="Left channel")
-plt.plot(peaks, beat[peaks], "x")
-#right channel? just incase idk
-#plt.plot(data[:, 1], label="Right channel")
-plt.legend()
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
+    #left channel
+    beat = data[:, 0]
+    #prominence is the steepness  
+    peaks,_ = find_peaks(beat, distance=distance, prominence=prominence)
+    np.diff(peaks)
 
 
+    #prominences = peak_prominences(beat, peaks)[0]
+    #print(prominences)
+
+    return peaks, beat
+    
+    
+def main():
+    #read from directory
+    data_dir = pjoin(dirname(__file__), 'heartbeat-01a.wav')
+    #read wav
+    samplerate, data = wavfile.read(data_dir)
+
+    peaks,beat = peak_count(data,2000,8e+08)
+    display_peaks(peaks,beat)
+
+if __name__ == "__main__":
+    main()
