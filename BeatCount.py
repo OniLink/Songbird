47143765fc2ecdcf0918bab43b791c6093a3f610
K-Concept <Kagaminelove02@gmail.com>
2022-03-06 13:21:49 -0800

    Updated BeatCount.py
    Added bpm stuff

--- BeatCount.py
+++ BeatCount.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 #code referenced https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
 #code referenced https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
+#code referenced http://www.paulvangent.com/2016/03/15/analyzing-a-discrete-heart-rate-signal-using-python-part-1/ 
 
 #This display orange xs where the peaks are 
 def display_peaks(peaks,beat):
@@ -21,7 +22,7 @@ def display_peaks(peaks,beat):
     plt.ylabel("Amplitude")
     plt.show()
 
-#takes in data, distance, prominence
+#takes in desired data, distance, prominence
 def peak_count(data,distance,prominence):
     #  data stuff
     #  print(f"number of channels = {data.shape[1]}")
@@ -34,12 +35,25 @@ def peak_count(data,distance,prominence):
     peaks,_ = find_peaks(beat, distance=distance, prominence=prominence)
     np.diff(peaks)
 
-
     #prominences = peak_prominences(beat, peaks)[0]
     #print(prominences)
 
     return peaks, beat
-    
+
+#calculates average bpm
+def bpm(samplerate,beat,peaks):
+    birdbeats_list = []
+    cnt = 0
+
+    while (cnt < (len(peaks)-1)):
+        birdbeats_interval = (peaks[cnt+1] - peaks[cnt]) #Calculate distance between beats in # of samples
+        ms_dist = ((birdbeats_interval / samplerate) * 1000.0) #Convert sample distances to ms distances
+        birdbeats_list.append(ms_dist) #Append to list
+        cnt += 1
+
+    bpm = 60000 / np.mean(birdbeats_list) #60000 ms (1 minute) / average interval of signal
+    print ("Average Heart Beat is: %.01f" %bpm)  #Round off to 1 decimal and print
+
     
 def main():
     #read from directory
@@ -48,7 +62,9 @@ def main():
     samplerate, data = wavfile.read(data_dir)
 
     peaks,beat = peak_count(data,2000,8e+08)
+    bpm(samplerate,beat,peaks)
     display_peaks(peaks,beat)
+    
 
 if __name__ == "__main__":
     main()
