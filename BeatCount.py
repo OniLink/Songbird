54c07313e3d90496cd40b6e02a0c15509d5fea02
megan black <blackme@oregonstate.edu>
2022-03-11 11:40:53 -0800

    Segments work with clean data

--- BeatCount.py
+++ BeatCount.py
@@ -5,6 +5,7 @@ from scipy.signal import find_peaks, peak_prominences
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import math
 #code referenced https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html
 #code referenced https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.find_peaks.html
 #code referenced http://www.paulvangent.com/2016/03/15/analyzing-a-discrete-heart-rate-signal-using-python-part-1/ 
@@ -24,11 +25,6 @@ def display_peaks(peaks,beat):
 
 #takes in desired data, distance, prominence
 def peak_count(data,distance,prominence):
-    #  data stuff
-    #  print(f"number of channels = {data.shape[1]}")
-    #  length = data.shape[0] / samplerate
-    #  print(f"length = {length}s")
-
     #left channel
     beat = data[:, 0] # for clean .wav, add make it data[:, 0]
 
@@ -36,8 +32,8 @@ def peak_count(data,distance,prominence):
     peaks,_ = find_peaks(beat, distance=distance, prominence=prominence)
     np.diff(peaks)
 
-    prominences = peak_prominences(beat, peaks)[0]
-    print(prominences)
+    # prominences = peak_prominences(beat, peaks)[0]
+    # print(prominences)
 
     return peaks, beat
 
@@ -58,12 +54,32 @@ def bpm(samplerate,beat,peaks):
     
 def main():
     #read from directory
-    data_dir = pjoin(dirname(__file__), 'heartbeat-01a.wav')
+    file_name = 'heartbeat-01a.wav'
+    # file_name = 'Sample-Filtered_out_000.wav'
+    data_dir = pjoin(dirname(__file__), file_name)
     #read wav
-    samplerate, data = wavfile.read(data_dir)
-    peaks,beat = peak_count(data,200,8e+08) #with clean data, 200, 8e+08 args
-    # bpm(samplerate,beat,peaks)
-    display_peaks(peaks,beat)
+    sample_rate, data = wavfile.read(data_dir)
+
+    # necessary variables for segmenting
+    total_length = len(data) / float(sample_rate)
+    segment_length = 60
+    start = 0
+    end = sample_rate * segment_length
+    for i in range(0, math.ceil(total_length/segment_length)):
+        segment = data[start:end]
+        
+        # try and calculate distance and prominence arguments 
+        distance = 2000 # calculate_distance(segment)
+        prominence = 8e+08 # calculate_prominence(segment)
+        peaks, beat = peak_count(segment,distance,prominence)
+        bpm(sample_rate,beat,peaks)
+        # display_peaks(peaks,beat)
+
+        start += sample_rate
+        end += sample_rate
+        if(end + sample_rate > len(data)): 
+            end = sample_rate
+
     
 
 if __name__ == "__main__":
